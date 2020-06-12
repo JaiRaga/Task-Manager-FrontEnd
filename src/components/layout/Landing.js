463cb40cf37566b5e153620786aaaf06a6be1657
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-12 09:12:14 +0530

    refactored navbar to its own component

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,49 +1,8 @@
 import React, { Fragment } from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  makeStyles,
-  Typography,
-  Button
-} from "@material-ui/core";
-import HomeIcon from "@material-ui/icons/Home";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
+import NavBar from "./NavBar";
 
 const Landing = () => {
-  const classes = useStyles();
-  console.log(classes);
-  return (
-    <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'>
-            <HomeIcon />
-          </IconButton>
-          <Typography variant='h6' className={classes.title}>
-            Tasker
-          </Typography>
-          <Button color='inherit'>Login</Button>
-          <Button color='inherit'>Register</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+  return <NavBar />;
 };
 
 export default Landing;
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  makeStyles,
+  Typography,
+  Button
+} from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'>
+            <HomeIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            Tasker
+          </Typography>
+          <Button color='inherit'>Login</Button>
+          <Button color='inherit'>Register</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default NavBar;
